9fc1e954c6d59f4b2936917581f963798448aa2a
Peter Oyerinde <peteroluwa2019@gmail.com>
2025-07-17 14:52:42 +0100

    factored db connecction

--- database.py
+++ database.py
@@ -11,15 +11,15 @@ password = os.getenv("DB_PASS")
 host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 db = os.getenv("DB_NAME")
-ca_path = os.getenv("DB_CA")
+ca_path = os.getenv("DB_CA", "ca.pem")  
 on_render = os.getenv("RENDER", "false").lower() == "true"
+
 
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}?ssl_ca={ca_path}"
 )
 
-connect_args = {"ssl": {"ca": ca_path}} if not on_render else {}
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
